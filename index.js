36e4dd19e96e8f3ae643682afd85827128f41cfe
Nikhilesh Shah <nikhileshshah10@gmail.com>
2022-06-16 16:27:24 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+var typing = new Typed('#mytext', //it will take 2 parameter , 1st html attribute and 2nd properties of animations
+
+    {
+        strings: ['Nik Animations','Wildlife-Edition❤️'],
+        typeSpeed: 100,
+        showCursor: false
+    }
+)
+AOS.init(
+    {
+        duration:1000
+    }
+);
